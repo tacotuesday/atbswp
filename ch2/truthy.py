14685238d3bd45ec004e860cd2381db48fce2d12
Grafton Cook <grafton.cook@gmail.com>
2017-12-22 13:14:16 -0500

    Added files via upload.

---
+++ ch2/truthy.py
@@ -0,0 +1,9 @@
+name = ''
+while not name:     # Because the empty string is False, this actually reads "While True:"
+    print('Enter your name:')
+    name = input()
+print('How many guests will you have?')
+numOfGuests = int(input())
+if numOfGuests:
+    print('Be sure to have enough room for all your guests.')
+print('Done')
